1305753ae515e74d801ab07e1d6653754f93e0f5
jonatan.lara <jonatan.lara@gft.com>
2022-03-25 14:07:38 +0100

    Inclusión de la barra de navegación y routing básico

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation-container">
+        <Link to="/" className="logo-container">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/shop" className="nav-link">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
